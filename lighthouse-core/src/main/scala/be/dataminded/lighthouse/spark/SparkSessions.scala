66575a646ca3ff4d2d6b0c16a8711faf2e2be45c
Mathias Lavaert <mathias.lavaert@dataminded.be>
2018-01-05 13:39:52 +0100

    Added core utils to the main module

---
+++ lighthouse-core/src/main/scala/be/dataminded/lighthouse/spark/SparkSessions.scala
@@ -0,0 +1,31 @@
+package be.dataminded.lighthouse.spark
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Mixin provides the application with a [[SparkSession]].
+  */
+trait SparkSessions {
+
+  /**
+    * Custom Spark configuration for the job.
+    */
+  val sparkOptions: Map[String, String] = Map.empty
+
+  private val defaultConfiguration: Map[String, String] = Map(
+    "spark.driver.memory"                                    -> "2g",
+    "spark.serializer"                                       -> "org.apache.spark.serializer.KryoSerializer",
+    "spark.sql.avro.compression.codec"                       -> "snappy",
+    "spark.sql.parquet.compression.codec"                    -> "snappy",
+    "spark.sql.sources.partitionColumnTypeInference.enabled" -> "false"
+  )
+
+  lazy val spark: SparkSession =
+    (defaultConfiguration ++ sparkOptions)
+      .foldLeft(SparkSession.builder()) {
+        case (b, (key, value)) => b.config(key, value)
+      }
+      .enableHiveSupport()
+      .getOrCreate()
+
+}
