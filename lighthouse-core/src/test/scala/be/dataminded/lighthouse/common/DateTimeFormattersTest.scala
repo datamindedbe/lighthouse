e79db0a9f6d21dd4a8c0783b6d0fab715cfcb9ac
Mathias Lavaert <mathias.lavaert@dataminded.be>
2018-01-13 23:00:18 +0100

    Integrage SCoverage in our build pipeline

--- lighthouse-core/src/test/scala/be/dataminded/lighthouse/common/DateTimeFormattersTest.scala
+++ lighthouse-core/src/test/scala/be/dataminded/lighthouse/common/DateTimeFormattersTest.scala
@@ -10,12 +10,12 @@ class DateTimeFormattersTest extends FunSuite with Matchers {
   test("Simple Date Format should format a date like yyyy/MM/dd") {
     val result = SimpleDateFormat.format(LocalDate.of(1990, 1, 1))
 
-    result should equal ("1990/01/01")
+    result should equal("1990/01/01")
   }
 
   test("Partitioned Date Format should format a date like year=yyyy/month=MM/day=dd") {
-    val result = PartitionedDateFormat.format(LocalDate.of(1990, 1,1))
+    val result = PartitionedDateFormat.format(LocalDate.of(1990, 1, 1))
 
-    result should equal ("year=1990/month=01/day=01")
+    result should equal("year=1990/month=01/day=01")
   }
 }
