1ef6ad8d90f2438b025076d7ad53165033f0b3d6
Mathias Lavaert <mathias.lavaert@dataminded.be>
2018-01-26 21:03:36 +0100

    Fix tests + tests for SingleFileSink

--- lighthouse-core/src/test/scala/be/dataminded/lighthouse/pipeline/SingleFileSinkSpec.scala
+++ lighthouse-core/src/test/scala/be/dataminded/lighthouse/pipeline/SingleFileSinkSpec.scala
@@ -2,9 +2,9 @@ package be.dataminded.lighthouse.pipeline
 
 import be.dataminded.lighthouse.testing.SharedSparkSession
 import better.files._
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{BeforeAndAfterEach, FunSpec, Matchers}
 
-class SingleFileSinkSpec extends FunSpec with SharedSparkSession with Matchers {
+class SingleFileSinkSpec extends FunSpec with SharedSparkSession with Matchers with BeforeAndAfterEach {
 
   import spark.implicits._
 
@@ -13,9 +13,11 @@ class SingleFileSinkSpec extends FunSpec with SharedSparkSession with Matchers {
     it("should write a DataFrame into a single partition") {
       val data = Seq("datadata", "datadatadata").toDF("single").repartition(2)
 
-      SparkFunction.of(data).makeSnapshot(TextSink("./target/output/text")).run(spark)
+      SparkFunction.of(data).makeSnapshot(SingleFileSink(TextSink("./target/output/text"))).run(spark)
 
       ("target" / "output" / "text").glob("*.txt").size should equal(1)
     }
   }
+
+  override protected def afterEach(): Unit = ("target" / "output" / "text").delete(true)
 }
