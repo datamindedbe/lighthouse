55009544aa0ab69d152529e6f501e8254d654f59
Mathias Lavaert <mathias.lavaert@dataminded.be>
2018-01-30 21:10:44 +0100

    Fixed a typo in test Models

--- lighthouse-core/src/test/scala/be/dataminded/lighthouse/Models.scala
+++ lighthouse-core/src/test/scala/be/dataminded/lighthouse/Models.scala
@@ -3,6 +3,6 @@ package be.dataminded.lighthouse
 object Models {
   case class RawPerson(name: String, age: Int)
   case class BasePerson(firstName: String, lastName: String, age: Int)
-  case class RawCustomer(ID: String, firstName: String, lastName: String, yearOfBirth: String)
+  case class RawCustomer(id: String, firstName: String, lastName: String, yearOfBirth: String)
   case class RawOrders(id: String, customerId: String)
 }
