152caa5ad70c7fbd3b08529010f33d8ab6cb65b0
michieldhadamus <39131088+michieldhadamus@users.noreply.github.com>
2018-07-31 16:10:24 +0200

    Added AvroDataLink extending PathBasedDataLink to read Avro files (#10)
    * Added AvroDataLink extending PathBasedDataLink to read Avro files
    
    * Updated based on Mathias' comments

---
+++ lighthouse-core/src/main/scala/be/dataminded/lighthouse/datalake/AvroDataLink.scala
@@ -0,0 +1,26 @@
+package be.dataminded.lighthouse.datalake
+
+import org.apache.spark.sql.{DataFrame, Dataset, SaveMode}
+
+class AvroDataLink(val path: LazyConfig[String],
+                   saveMode: SaveMode = SaveMode.Overwrite,
+                   partitionedBy: List[String] = List.empty,
+                   options: Map[String, String] = Map.empty)
+    extends PathBasedDataLink {
+
+  override def doRead(path: String): DataFrame = {
+    spark.read
+      .format("com.databricks.spark.avro")
+      .options(options)
+      .load(path)
+  }
+
+  override def doWrite[T](dataset: Dataset[T], path: String): Unit = {
+    dataset.write
+      .format("com.databricks.spark.avro")
+      .partitionBy(partitionedBy: _*)
+      .options(options)
+      .mode(saveMode)
+      .save(path)
+  }
+}
